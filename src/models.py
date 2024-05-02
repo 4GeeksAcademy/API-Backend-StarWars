0732e2f2512ae1bd6fe77b1ed349530957f57617
Orianac0698 <150297944+Orianac0698@users.noreply.github.com>
2024-05-02 17:48:09 +0000

    Modelado de Datos

--- src/models.py
+++ src/models.py
@@ -6,7 +6,9 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(25), nullable=False)
+    favorite = db.relationship("Favorite")
+
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -15,5 +17,78 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "username": self.username,
             # do not serialize the password, its a security breach
+        }
+
+class Character(db.Model):
+   
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    heigth = db.Column(db.Integer)
+    mass = db.Column(db.Integer)
+    hair_color = db.Column(db.String(10))
+    skin_color = db.Column(db.String(10))
+    eye_color= db.Column(db.String(10))
+    birth_year = db.Column(db.String(10))
+    gender = db.Column(db.String(10))
+    favorite = db.relationship("Favorite")
+
+
+    def __repr__(self):
+        return '<Character %r>' % self.name
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "heigth": self.heigth,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+        }
+    
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    diameter = db.Column(db.Integer)
+    rotation_period = db.Column(db.Integer)
+    orbital_period = db.Column(db.Integer)
+    gravity = db.Column(db.String(20))
+    population= db.Column(db.String(10))
+    favorite = db.relationship("Favorite")
+
+    
+    def __repr__(self):
+        return '<Planet %r>' % self.name
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "gravity": self.gravity,
+            "population": self.population,
+        }
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_Id = db.Column(db.Integer, db.ForeignKey("user.id")) 
+    character_Id = db.Column(db.Integer, db.ForeignKey("character.id"))
+    planet_Id = db.Column(db.Integer, db.ForeignKey("planet.id"))
+
+    def __repr__(self):
+        return '<Favorite %r>' % self.id
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_Id": self.user_Id,
+            "character_Id": self.character_Id,
+            "planet_Id": self.planet_Id,
         }
